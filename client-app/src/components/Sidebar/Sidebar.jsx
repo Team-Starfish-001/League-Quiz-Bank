319b80f1a3d8eb8f6f5e94e66b0f92ed38bfc212
Ollie Pai <ollie.pai2003@gmail.com>
2018-10-28 14:28:59 -0700

    added some framework

--- client-app/src/components/Sidebar/Sidebar.jsx
+++ client-app/src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Sidebar extends React.Component {
     render() {
+        var pages = ["Blah", "Blah", "Blah", "Blah"];
 
+        return (
+            <div className="sidebar">
+                <ul>
+
+                </ul>
+            </div>
+        )
     }
 }
 
