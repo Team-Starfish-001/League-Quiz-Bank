98089dde8aa53f7e55b8ee42ff07c7255981b214
League <league@WTS8.attlocal.net>
2018-10-28 14:29:49 -0700

    Adjusted Header

--- client-app/src/components/Dashboard/Header.jsx
+++ client-app/src/components/Dashboard/Header.jsx
@@ -1,11 +1,17 @@
 import  React from 'react';
 
+var headerStyle = {
+    "background":"orange",
+    "color":"white",
+    "width":"100%",
+    "position":"absolute"
+};
 class Header extends React.Component {
 
     render(){
 	return(
-	       <div className='dashboard'>
-                
+	       <div className='header' style={headerStyle}>
+                <center><h2>League Quiz App</h2></center>
 	       </div>
 	       );
    }
