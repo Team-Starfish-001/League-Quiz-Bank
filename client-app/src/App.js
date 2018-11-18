ff5ab153734a5a3825c6e9800ce91b221e0da2f0
League <league@WTS8.attlocal.net>
2018-11-18 14:17:57 -0800

    added QuizList component

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,24 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard.jsx';
-import Quiz from './components/Quiz/Quiz.jsx';
-
-const data = {
-  "QuizName": "SuperQuiz",
-  "QuizDate": "Sunday, November 10",
-  "QuizImg": logo,
-  "Teacher": "Bob",
-  "ClassName": "Frogs",
-  "ClassLevel": "Level 2",
-  "QuizId": "1"
+import QuizList from './components/QuizList/QuizList.jsx';
+var data =[{
+	"QuizName": "SuperQuiz",
+	"QuizDate": "Sunday, November 10",
+       "QuizImg": logo,
+	"Teacher": "Bob",
+	"ClassName": "Frogs",
+	"ClassLevel": "Level 2",
+	"QuizId": "1"
+    },{
+    "QuizName":"sadsad",
+    "QuizDate":"Sundatsad",
+    "QuizImg": logo,
+    "Teacher": "Bob",
+    "ClassName": "Frogs",
+    "ClassLevel": "Level 2",
+    "QuizId": "1"
+    },{ 
+    "QuizName":"asd",
+    "QuizDate":"asdas",
+    "QuizImg": logo,
+    "Teacher": "Bob",
+    "ClassName": "Frogs",
+    "ClassLevel": "Level 2",
+    "QuizId": "1"
 }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Dashboard/>
-      <Quiz data={data} />
+      <QuizList data={data} />
       </div>
     );
   }
