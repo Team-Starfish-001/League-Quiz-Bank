f99d417fbb0d4c9a65568d535d4fc7fbcf5a170c
League <league@WTS20.attlocal.net>
2019-01-06 14:09:23 -0800

    Finished Student Quiz View

--- client-app/src/components/QuestionObject/QuestionObject.jsx
+++ client-app/src/components/QuestionObject/QuestionObject.jsx
@@ -7,27 +7,51 @@ class QuestionObject extends React.Component {
 		case "single":
 		     return(
 			<form id={this.props.data.index}>
-			  {this.props.data.choices.map((e)=>{
-			  <input type="radio" name={this.props.data.index}/>
-			  })}
+			  {this.props.data.choices.map((e)=>
+				<div>
+				<input type="radio" value={e.answer} name={this.props.data.index}/>   {e.answer}
+				 <br/>
+				 </div>
+			)}
   	 	      	</form> 
 		     );
 		break;
 		case "multiple":
+		     return(
+                        <form id={this.props.data.index}>
+                          {this.props.data.choices.map((e)=>
+                                <div>
+                                <input type="checkbox" value={e.answer} name={this.props.data.index}/>   {e.answer}
+                                 <br/>
+                                 </div>
+                        )}
+                        </form>
+                     );
 		break;
 		case "fill":
+		     return(
+                        <form id={this.props.data.index}>
+                       	      <textarea name="input" cols="40" rows="5"></textarea>    
+                        </form>
+                     );
 		break;
 		default:
+		     return(
+                        <form id={this.props.data.index}>
+				      <textarea name="input" cols="40" rows="5"></textarea>
+                        </form>
+                     );
+
 		}
 		
       }
      render(){
         return(
                 <div>
-			<h2>#{this.props.data.index}. </h2>
-			<h3>{this.props.data.prompt}</h3>
+			<h3>#{this.props.data.index}: {this.props.data.prompt}</h3>
 			<br/>
-			{createForm()}
+			{this.createForm()}
+			<br/>
                 </div>
         );
       }
--- client-app/src/components/QuizObject/QuizObject.jsx
+++ client-app/src/components/QuizObject/QuizObject.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import QuestionObject from '../QuestionObject/QuestionObject';
 class QuizObject extends React.Component {
+      submitForms(){
+      
+      }
       render(){
 	return(
 		<div>
 		{this.props.data.map((e)=><QuestionObject data={e}/>)}
+		<input type="button" value="Submit" onclick={this.submitForms()} />
 		</div>
 	);
       }
