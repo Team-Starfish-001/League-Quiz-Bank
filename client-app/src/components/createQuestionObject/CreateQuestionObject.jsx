8f004daecc45a87d0fcf5b66325afd0d7f5ee2b4
League <league@WTS8.attlocal.net>
2019-02-24 14:23:33 -0800

    added functionality to answer prompt

--- client-app/src/components/createQuestionObject/CreateQuestionObject.jsx
+++ client-app/src/components/createQuestionObject/CreateQuestionObject.jsx
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 
 class CreateQuestionObject extends Component {
+      constructor(props){
+	super(props);
+	this.state = {selectedValue:"single"};
+      }
+      handleChange(e){
+      let a = e.target.value;
+      this.setState({
+		"selectedValue":a
+		});
+      }
+      getAnswerPrompt(){
+	switch(this.state.selectedValue){
+		case "fill":
+		     return(<p>sadasd</p>);
+		break;
+		case "single":
+		     return(<p>singesfsfsdfds</p>);
+		break;
+		case "multiple":
+		     return(<p>yeyeyeyeeaf</p>);
+	}
+      }
       render(){
 	return(
 		<div>
 			<form id="1">
 		      	      Question: <input type="text" name="Question"/><br/>
-			      Question Type: <select name="questionType">
+			      Question Type: <select name="questionType" value={this.state.selectValue} 
+        		      onChange={this.handleChange}>
 				<option value="single" selected>Single Choice Answer</option>
 				<option value="multiple">Multiple Choice Answer</option>
 				<option value="fill">Fill in Answer</option>
 			      </select><br/>
-			      
+			      {this.getAnswerPrompt()}
 			</form>
 			</div>
 		);
