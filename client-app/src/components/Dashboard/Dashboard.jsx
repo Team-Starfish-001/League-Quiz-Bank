8120b326eed9331bfe87ecd8e26205c17415b7a7
Robbie Hinman <robbie.hinman@gmail.com>
2019-03-24 13:46:32 -0700

    Got it working and changed data file names

--- client-app/src/components/Dashboard/Dashboard.jsx
+++ client-app/src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Header from '../Header/Header.jsx';
 // import Content from '../Content/Content.jsx';
-import Sidebar from '../Sidebar/Sidebar.jsx';
 import DataList from '../DataList/DataList';
-import { dummyData } from '../../mockData/DataListData';
+import dataListData from '../../mockData/DataListData';
 
 var DashboardStyle = {};
 
@@ -11,8 +10,8 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className='dashboard' style={DashboardStyle}>
-        <Header />
-        <Sidebar />
+        <Header />\
+        <DataList data={dataListData} />
         {/* <Content data={this.props.data}/> */}
       </div>
     );
