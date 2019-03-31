fdfeb0a42a07914b7f87068bc2c70690a9ddcbca
cheetah676 <league@WTS20.attlocal.net>
2019-03-31 13:54:02 -0700

    Added post route

--- api/server.js
+++ api/server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
 
-var data = {}
+app.use(bodyparser.json())
+bodyparser.urlencoded({
+	extended: true
+})
 
 app.get('/getQuiz', (req, res) => {
-	res.send(data)
+	res.send({})
+})
+
+app.post('getQuiz', (req,res) => {
+	var data = req.body
 })
 
 app.listen(8080, () => console.log("Server Started on Port 8080!!!"))
