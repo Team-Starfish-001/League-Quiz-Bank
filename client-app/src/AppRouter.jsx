d373f546540c9f3a656b09648fc62e42c9f8ecb9
Ollie Pai <ollie.pai2003@gmail.com>
2019-04-07 14:25:05 -0700

    add login view page

--- client-app/src/AppRouter.jsx
+++ client-app/src/AppRouter.jsx
@@ -3,12 +3,14 @@ import { Router, Route, Switch } from 'react-router';
 import createHistory from 'history/createBrowserHistory';
 import StudentQuizView from './components/StudentTakeAQuizView/StudentQuizView';
 import Dashboard from './components/Dashboard/Dashboard';
+import LoginView from './components/LoginView/LoginView';
 
 function AppRouter() {
   return (
     <div>
       <Router history={createHistory()}>
         <Switch>
+          <Route path="/login" exact component={LoginView} />
           <Route path="/" exact component={Dashboard} />
           <Route path="/quiz" exact component={StudentQuizView} />
         </Switch>
