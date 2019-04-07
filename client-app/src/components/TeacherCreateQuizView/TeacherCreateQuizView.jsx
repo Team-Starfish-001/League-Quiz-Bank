2a6b2717b1f0cb9413042967d68d2d94085c3976
Sasanka742 <league@WTS20.attlocal.net>
2019-04-07 14:31:16 -0700

    added some stuff

--- client-app/src/components/TeacherCreateQuizView/TeacherCreateQuizView.jsx
+++ client-app/src/components/TeacherCreateQuizView/TeacherCreateQuizView.jsx
@@ -5,7 +5,14 @@ class TeacherCreateQuizView extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			   questionAmtSelectedValue: 10
+			   questionAmtSelectedValue: 10,
+			   form_data: {
+				title: "",
+				desc: "",
+				tags: "",
+				amt: 10,
+				questions: null
+			}
 		};
 	}
 
@@ -14,9 +21,34 @@ class TeacherCreateQuizView extends Component {
 		if(amt > 100 || amt < 1){
 			amt = 10
 		}
+		var questions= [];
+		var questionBase={id: 1,
+			questionType:"single",
+			question:"",
+			answerOne:"",
+			answerTwo:"",
+			optionOne:"",
+			optionTwo:"",
+			optionThree:""}
+		for(var i = 1; i < amt +1; i++){
+			questionBase.id = i;
+			questions.push(questionBase)
+		}
 		this.setState({
-			questionAmtSelectedValue: amt
+			questionAmtSelectedValue: amt,
+			form_data: {
+				title: this.state.form_data.title,
+				desc: this.state.form_data.desc,
+				tags: this.state.form_data.tags,
+				amt: amt,
+				questions: questions
+			}
 		});
+		console.log(this.state)
+	}
+
+	handleQuestion=(_state)=>{
+		console.log(_state)
 	}
 
 	submitForms =()=>{
@@ -32,7 +64,7 @@ class TeacherCreateQuizView extends Component {
 					Desc: <input type="text" name="Desc"/><br/>
 					Tags: <input type="text" name="Tags"/><br/>
 				</form><br/>
-				{Array.from(Array(this.state.questionAmtSelectedValue).keys()).map((e)=><CreateQuestionObject index={e+1} />)}
+				{Array.from(Array(this.state.questionAmtSelectedValue).keys()).map((e)=><CreateQuestionObject index={e+1} handleQuestion={this.handleQuestion} />)}
 				<input type="submit" className="button" value="Submit" onClick={this.submitForms()}/>
 			</div>
 			
--- client-app/src/components/createQuestionObject/CreateQuestionObject.jsx
+++ client-app/src/components/createQuestionObject/CreateQuestionObject.jsx
@@ -3,30 +3,68 @@ import React, { Component } from 'react';
 class CreateQuestionObject extends Component {
       constructor(props){
 		super(props);
-		this.state = {selectedValue:"single"};
-      }
-      onChange =(e)=>{
-      	this.setState({selectedValue:e.target.value})
-      }
+		this.state = {
+			id: this.props.index,
+			questionType:"single",
+			question:"",
+			answerOne:"",
+			answerTwo:"",
+			optionOne:"",
+			optionTwo:"",
+			optionThree:""
+		};
+	  }
+	  QuestionChange(){
+		this.props.handleQuestion(this.state);
+	  }
+      onChangeType =(e)=>{
+		  this.setState({
+			  questionType:e.target.value,
+			  answerOne:"",
+			answerTwo:"",
+			optionOne:"",
+			optionTwo:"",
+			optionThree:""
+		}, this.QuestionChange)
+	  }
+	  onChangequestion =(e)=>{
+		this.setState({question:e.target.value}, this.QuestionChange)
+		}
+		onChangeAnswerOne =(e)=>{
+			this.setState({answerOne:e.target.value}, this.QuestionChange)
+		}
+		onChangeAnswerTwo =(e)=>{
+			this.setState({answerTwo:e.target.value}, this.QuestionChange)
+		}
+		onChangeOptionOne =(e)=>{
+			this.setState({optionOne:e.target.value}, this.QuestionChange)
+		}
+		onChangeOptionTwo =(e)=>{
+			this.setState({optionTwo:e.target.value}, this.QuestionChange)
+		}
+		onChangeOptionThree =(e)=>{
+			this.setState({optionThree:e.target.value}, this.QuestionChange)
+		}
       getAnswerPrompt(){
-		switch(this.state.selectedValue){
+		switch(this.state.questionType){
 			case "fill":
-			return(
-				<div>Answer: <input type="text" name="answer-fill"/><br/></div>
-				);
+			return(<div>
+				Answer:  <input type="text"  name="answer-1" value={this.state.answerOne} onChange={this.onChangeAnswerOne}/>
+				<br/>
+				</div>);
 			case "single":
 			return(<div>
-				Answer: <input type="text" name="answer-single"/><br/>
-				Option 1: <input type="text" name="option-single-1"/><br/>
-				Option 2: <input type="text" name="option-single-2"/><br/>
-				Option 3: <input type="text" name="option-single-3"/><br/>
+				Answer:   <input type="text" name="answer-1" value={this.state.answerOne} onChange={this.onChangeAnswerOne}/><br/>
+				Option 1: <input type="text" name="option-1" value={this.state.optionOne} onChange={this.onChangeOptionOne}/><br/>
+				Option 2: <input type="text" name="option-2" value={this.state.optionTwo} onChange={this.onChangeOptionTwo}/><br/>
+				Option 3: <input type="text" name="option-3" value={this.state.optionThree} onChange={this.onChangeOptionThree}/><br/>
 				</div>);
 			case "multiple":
 			return(<div>
-				Answer 1: <input type="text" name="answer-multiple-1"/><br/>
-				Answer 2: <input type="text" name="answer-multiple-2"/><br/>
-				Option 1: <input type="text" name="option-multiple-1"/><br/>
-				Option 2: <input type="text" name="option-multiple-2"/><br/>
+				Answer 1: <input type="text" name="answer-1" value={this.state.answerOne} onChange={this.onChangeAnswerOne}/><br/>
+				Answer 2: <input type="text" name="answer-2" value={this.state.answerTwo} onChange={this.onChangeAnswerTwo}/><br/>
+				Option 1: <input type="text" name="option-1" value={this.state.optionOne} onChange={this.onChangeOptionOne}/><br/>
+				Option 2: <input type="text" name="option-2" value={this.state.optionTwo} onChange={this.onChangeOptionTwo}/><br/>
 				</div>);
 			default:
 				return(null);
@@ -37,9 +75,9 @@ class CreateQuestionObject extends Component {
 			<div>
 				<h4>{this.props.index}.</h4>
 				<form id={this.props.index}>
-					Question: <input type="text" name="Question"/><br/>
-					Question Type: <select name="questionType" value={this.state.selectValue} 
-					onChange={this.onChange}>
+					Question: <input type="text" name="Question" onChange={this.onChangequestion}/><br/>
+					Question Type: <select name="questionType" value={this.state.questionType} 
+					onChange={this.onChangeType}>
 					<option value="single" selected>Single Choice Answer</option>
 					<option value="multiple">Multiple Choice Answer</option>
 					<option value="fill">Fill in Answer</option>
