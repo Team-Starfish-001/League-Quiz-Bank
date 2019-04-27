5307ae2ba7d5d734f8e9d0632a7f1ab7e38b21c5
Robbie Hinman <robbie.hinman@gmail.com>
2019-04-27 00:45:23 -0700

    swapping router for browserRouter

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import StudentQuizView from './components/StudentTakeAQuizView/StudentQuizView';
 import TeacherCreateQuizView from './components/TeacherCreateQuizView/TeacherCreateQuizView';
 import Dashboard from './components/Dashboard/Dashboard';
@@ -10,12 +9,10 @@ const basename = process.env.REACT_APP_BASENAME;
 function AppRouter() {
   return (
     <div>
-      <Router history={createBrowserHistory()} basename={basename}>
-        <Switch>
-          <Route path='/' exact component={Dashboard} />
-          <Route path='/takeQuiz' exact component={StudentQuizView} />
-          <Route path='/makeQuiz' exact component={TeacherCreateQuizView} />
-        </Switch>
+      <Router basename={basename}>
+        <Route path='/' exact component={Dashboard} />
+        <Route path='/takeQuiz' exact component={StudentQuizView} />
+        <Route path='/makeQuiz' exact component={TeacherCreateQuizView} />
       </Router>
     </div>
   );
