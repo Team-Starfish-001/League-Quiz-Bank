7103dddd82ff801b73870140b6343cb5d4ce89cb
Sasanka742 <league@WTS20.attlocal.net>
2019-04-28 13:57:11 -0700

    added a makeshift db

--- api/server.js
+++ api/server.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+    const fs = require('fs')
 const app = express()
+
+var data = JSON.parse(fs.readFileSync('quizzes.json'))
 
 app.use(bodyparser.json())
 bodyparser.urlencoded({
 	extended: true
 })
 
+
 app.get('/getQuiz', (req, res) => {
-	res.send({})
+	res.send(data)
 })
 
-app.post('getQuiz', (req,res) => {
-	var data = req.body
+app.post('/getQuiz', (req,res) => {
+	data.push(req.body)
+	fs.writeFileSync('quizzes.json', JSON.stringify(data));  
+	res.send(data)
 })
 
 app.listen(8080, () => console.log("Server Started on Port 8080!!!"))
