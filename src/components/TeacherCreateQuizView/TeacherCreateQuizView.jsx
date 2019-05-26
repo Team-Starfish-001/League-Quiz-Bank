d3e669323ebde2f2ace072146d1f7b1a212d9684
Ollie Pai <ollie.pai2003@gmail.com>
2019-05-26 14:28:02 -0700

    worked on styling create quiz page

--- src/components/TeacherCreateQuizView/TeacherCreateQuizView.jsx
+++ src/components/TeacherCreateQuizView/TeacherCreateQuizView.jsx
@@ -7,67 +7,71 @@ import CardContent from '@material-ui/core/CardContent';
 import CreateQuestionObject from '../createQuestionObject/CreateQuestionObject';
 
 const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-  },
-  dense: {
-    marginTop: 16,
-  },
-  menu: {
-    width: 200,
-  },
+	container: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
+	textField: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+	},
+	dense: {
+		marginTop: 16,
+	},
+	menu: {
+		width: 200,
+	},
 });
 class TeacherCreateQuizView extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		var list = []
-                for(var i = 1; i < 11; i++){
-			list.push({id: i,
-                        questionType:"single",
-                        question:"",
-                        answerOne:"",
-                        answerTwo:"",
-                        optionOne:"",
-                        optionTwo:"",
-                        optionThree:""})
+		for (var i = 1; i < 11; i++) {
+			list.push({
+				id: i,
+				questionType: "single",
+				question: "",
+				answerOne: "",
+				answerTwo: "",
+				optionOne: "",
+				optionTwo: "",
+				optionThree: ""
+			})
 		}
 		this.state = {
-                           questionAmtSelectedValue: 10,
-                           form_data: {
-                                title: "",
-                                desc: "",
-                                tags: "",
-                                amt: 10,
-                                questions: list
-                        }
-                };
+			questionAmtSelectedValue: 10,
+			form_data: {
+				title: "",
+				desc: "",
+				tags: "",
+				amt: 10,
+				questions: list
+			}
+		};
 	}
 
-  	onChange=(e)=>{
+	onChange = (e) => {
 		var amt = (parseInt(e.target.value)) ? parseInt(e.target.value) : 10;
 		var old_amt = this.state.form_data.amt
-		if(amt > 100 || amt < 1){
+		if (amt > 100 || amt < 1) {
 			amt = 10
 		}
-		var questions= [];
+		var questions = [];
 		var old_list = this.state.form_data.questions
-		for(var i = 1; i < amt +1; i++){
-			if(i-1 < old_amt){
-			     questions.push(old_list[i-1])
-			}else{
-				questions.push({id: i,
-                        			questionType:"single",
-                       		 		question:"",
-                        	  		answerOne:"",
-						answerTwo:"",
-                      		   		optionOne:"",
-                        	   		optionTwo:"",
-                        	   		optionThree:""})
+		for (var i = 1; i < amt + 1; i++) {
+			if (i - 1 < old_amt) {
+				questions.push(old_list[i - 1])
+			} else {
+				questions.push({
+					id: i,
+					questionType: "single",
+					question: "",
+					answerOne: "",
+					answerTwo: "",
+					optionOne: "",
+					optionTwo: "",
+					optionThree: ""
+				})
 			}
 		}
 		this.setState({
@@ -79,7 +83,7 @@ class TeacherCreateQuizView extends Component {
 		});
 	}
 
-	handleQuestion=(_state)=>{
+	handleQuestion = (_state) => {
 		var index = _state.id
 		var old_list = this.state.form_data.questions
 		var old_obj = old_list[index]
@@ -92,62 +96,65 @@ class TeacherCreateQuizView extends Component {
 		old_obj.optionTwo = _state.optionTwo
 		old_obj.optionThree = _state.optionThree
 		this.setState({
-			form_data:{
+			form_data: {
 				amt: amt,
 				questions: old_list
 			}
 		})
 	}
 
-	submitForms =()=>{
-		    console.log(this.state)
+	submitForms = () => {
+		console.log(this.state)
 	}
 
-    render(){
-	const { classes } = this.props;
-		return(
+	render() {
+		const { classes } = this.props;
+		return (
 			<div>
-				<h1>Create Quiz</h1>
-				<br/>
-				Amount of Questions: <input type="text" name="question_amt" onChange={this.onChange} /><br/>
 				<form id="data">
 					<TextField
-						id="outlined-name"
-						label="Title"				
+						id="title"
+						label="Title"
 						className={classes.textField}
-          					margin="normal"
-          					variant="outlined"
-        					/>
+						margin="normal"
+						variant="standard"
+						inputProps={{
+							style: {fontSize: 30} 
+						}}
+					/>
+					<br />
 					<TextField
-                                                id="outlined-name"
-                                                label="Description"
-                                                className={classes.textField}
-                                                margin="normal"
-                                                variant="outlined"
-                                                />
+						id="outlined-name"
+						label="Description"
+						className={classes.textField}
+						margin="normal"
+						variant="outlined"
+					/>
 					<TextField
-                                                id="outlined-name"
-                                                label="Class"
-                                                className={classes.textField}
-                                                margin="normal"
-                                                variant="outlined"
-                                                />
-				</form><br/>
+						id="outlined-name"
+						label="Class"
+						className={classes.textField}
+						margin="normal"
+						variant="outlined"
+					/>
+					<br />
+					<TextField name="question_amt" label="Amount of Questions" className={classes.textField} margin="normal" variant="outlined" onChange={this.onChange} />
+				</form><br />
 				<Card className={classes.card}>
 					<CardContent>
-						{Array.from(Array(this.state.questionAmtSelectedValue).keys()).map((e)=><CreateQuestionObject index={e+1} handleQuestion={this.handleQuestion} />)}
+						{Array.from(Array(this.state.questionAmtSelectedValue).keys()).map((e) => <CreateQuestionObject index={e + 1} handleQuestion={this.handleQuestion} />)}
 					</CardContent>
 				</Card>
-				<input type="submit" className="button" value="Submit" onClick={this.submitForms()}/>
+				<input type="submit" className="button" value="Submit" onClick={this.submitForms()} />
 
 			</div>
 
-	);
-      }
+		);
+	}
 }
 
 TeacherCreateQuizView.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(TeacherCreateQuizView);
--- src/components/createQuestionObject/CreateQuestionObject.jsx
+++ src/components/createQuestionObject/CreateQuestionObject.jsx
@@ -1,92 +1,111 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 class CreateQuestionObject extends Component {
-      constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
-			id: this.props.index -1,
-			questionType:"single",
-			question:"",
-			answerOne:"",
-			answerTwo:"",
-			optionOne:"",
-			optionTwo:"",
-			optionThree:""
+			id: this.props.index - 1,
+			questionType: "single",
+			question: "",
+			answerOne: "",
+			answerTwo: "",
+			optionOne: "",
+			optionTwo: "",
+			optionThree: ""
 		};
-	  }
-	  QuestionChange(){
+	}
+	QuestionChange() {
 		this.props.handleQuestion(this.state);
-	  }
-      onChangeType =(e)=>{
-		  this.setState({
-		        questionType:e.target.value,
-			answerOne:"",
-			answerTwo:"",
-			optionOne:"",
-			optionTwo:"",
-			optionThree:""
+	}
+	onChangeType = (e) => {
+		this.setState({
+			questionType: e.target.value,
+			answerOne: "",
+			answerTwo: "",
+			optionOne: "",
+			optionTwo: "",
+			optionThree: ""
 		}, this.QuestionChange)
-	  }
-	  onChangequestion =(e)=>{
-		this.setState({question:e.target.value}, this.QuestionChange)
-		}
-		onChangeAnswerOne =(e)=>{
-			this.setState({answerOne:e.target.value}, this.QuestionChange)
-		}
-		onChangeAnswerTwo =(e)=>{
-			this.setState({answerTwo:e.target.value}, this.QuestionChange)
-		}
-		onChangeOptionOne =(e)=>{
-			this.setState({optionOne:e.target.value}, this.QuestionChange)
-		}
-		onChangeOptionTwo =(e)=>{
-			this.setState({optionTwo:e.target.value}, this.QuestionChange)
-		}
-		onChangeOptionThree =(e)=>{
-			this.setState({optionThree:e.target.value}, this.QuestionChange)
-		}
-      getAnswerPrompt(){
-		switch(this.state.questionType){
+	}
+	onChangequestion = (e) => {
+		this.setState({ question: e.target.value }, this.QuestionChange)
+	}
+	onChangeAnswerOne = (e) => {
+		this.setState({ answerOne: e.target.value }, this.QuestionChange)
+	}
+	onChangeAnswerTwo = (e) => {
+		this.setState({ answerTwo: e.target.value }, this.QuestionChange)
+	}
+	onChangeOptionOne = (e) => {
+		this.setState({ optionOne: e.target.value }, this.QuestionChange)
+	}
+	onChangeOptionTwo = (e) => {
+		this.setState({ optionTwo: e.target.value }, this.QuestionChange)
+	}
+	onChangeOptionThree = (e) => {
+		this.setState({ optionThree: e.target.value }, this.QuestionChange)
+	}
+	getAnswerPrompt() {
+		switch (this.state.questionType) {
 			case "fill":
-			return(<div>
-				Answer:  <input type="text"  name="answer-1" value={this.state.answerOne} onChange={this.onChangeAnswerOne}/>
-				<br/>
+				return (<div>
+					<TextField name="answer-1" label="Answer" margin="normal" variant="outlined" value={this.state.answerOne} onChange={this.onChangeAnswerOne} />
+					<br />
 				</div>);
 			case "single":
-			return(<div>
-				Answer:   <input type="text" name="answer-1" value={this.state.answerOne} onChange={this.onChangeAnswerOne}/><br/>
-				Option 1: <input type="text" name="option-1" value={this.state.optionOne} onChange={this.onChangeOptionOne}/><br/>
-				Option 2: <input type="text" name="option-2" value={this.state.optionTwo} onChange={this.onChangeOptionTwo}/><br/>
-				Option 3: <input type="text" name="option-3" value={this.state.optionThree} onChange={this.onChangeOptionThree}/><br/>
+				return (<div>
+					<TextField name="answer-1" label="Answer" margin="normal" variant="outlined" value={this.state.answerOne} onChange={this.onChangeAnswerOne} />
+					<br />
+					<TextField name="option-1" label="Option 1" margin="normal" variant="outlined" value={this.state.optionOne} onChange={this.onChangeOptionOne} />
+					<br />
+					<TextField name="option-2" label="Option 2" margin="normal" variant="outlined" value={this.state.optionTwo} onChange={this.onChangeOptionTwo} />
+					<br />
+					<TextField name="option-3" label="Option 3" margin="normal" variant="outlined" value={this.state.optionThree} onChange={this.onChangeOptionThree} />
 				</div>);
 			case "multiple":
-			return(<div>
-				Answer 1: <input type="text" name="answer-1" value={this.state.answerOne} onChange={this.onChangeAnswerOne}/><br/>
-				Answer 2: <input type="text" name="answer-2" value={this.state.answerTwo} onChange={this.onChangeAnswerTwo}/><br/>
-				Option 1: <input type="text" name="option-1" value={this.state.optionOne} onChange={this.onChangeOptionOne}/><br/>
-				Option 2: <input type="text" name="option-2" value={this.state.optionTwo} onChange={this.onChangeOptionTwo}/><br/>
+				return (<div>
+					<TextField name="answer-1" label="Answer 1" margin="normal" variant="outlined" value={this.state.answerOne} onChange={this.onChangeAnswerOne} />
+					<br />
+					<TextField name="answer-2" label="Answer 2" margin="normal" variant="outlined" value={this.state.answerTwo} onChange={this.onChangeAnswerTwo} />
+					<br />
+					<TextField name="option-1" label="Option 1" margin="normal" variant="outlined" value={this.state.optionOne} onChange={this.onChangeOptionOne} />
+					<br />
+					<TextField name="option-2" label="Option 2" margin="normal" variant="outlined" value={this.state.optionTwo} onChange={this.onChangeOptionTwo} />
+					<br />
 				</div>);
 			default:
-				return(null);
+				return (null);
+		}
 	}
-      }
-      render(){
-		return(
+	render() {
+		return (
 			<div>
 				<h4>{this.props.index}.</h4>
 				<form id={this.props.index}>
-					Question: <input type="text" name="Question" onChange={this.onChangequestion}/><br/>
-					Question Type: <select name="questionType" value={this.state.questionType} 
-					onChange={this.onChangeType}>
-					<option value="single" >Single Choice Answer</option>
-					<option value="multiple">Multiple Choice Answer</option>
-					<option value="fill">Fill in Answer</option>
-					</select><br/>
+					<TextField name="Question" label="Question" margin="normal" variant="standard" inputProps={{ style: { fontSize: 20 } }} onChange={this.onChangequestion} />
+					<br />
+					<Select
+						value={this.state.questionType}
+						onChange={this.onChangeType}
+						inputProps={{
+							name: 'age',
+							id: 'age-simple',
+						}}
+					></Select>
+					Question Type: <select name="questionType" value={this.state.questionType}
+						onChange={this.onChangeType}>
+						<option value="single" >Single Choice Answer</option>
+						<option value="multiple">Multiple Choice Answer</option>
+						<option value="fill">Fill in Answer</option>
+					</select><br />
 					{this.getAnswerPrompt()}
 				</form>
-				<br/>
-				</div>
-			);
-      }
+				<br />
+			</div>
+		);
+	}
 }
 export default CreateQuestionObject;
