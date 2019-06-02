a5b80ed9ce2440a7460ac3462216bb48d10b6d66
Ollie Pai <ollie.pai2003@gmail.com>
2019-06-02 13:52:00 -0700

    formatting changes

--- src/components/TeacherCreateQuizView/TeacherCreateQuizView.jsx
+++ src/components/TeacherCreateQuizView/TeacherCreateQuizView.jsx
@@ -26,130 +26,148 @@ const styles = theme => ({
 class TeacherCreateQuizView extends Component {
   constructor(props) {
     super(props);
-    var list = []
+    var list = [];
     for (var i = 1; i < 11; i++) {
       list.push({
         id: i,
-        questionType: "single",
-        question: "",
-        answerOne: "",
-        answerTwo: "",
-        optionOne: "",
-        optionTwo: "",
-        optionThree: ""
-      })
+        questionType: 'single',
+        question: '',
+        answerOne: '',
+        answerTwo: '',
+        optionOne: '',
+        optionTwo: '',
+        optionThree: '',
+      });
     }
     this.state = {
       questionAmtSelectedValue: 10,
       form_data: {
-        title: "",
-        desc: "",
-        tags: "",
+        title: '',
+        desc: '',
+        tags: '',
         amt: 10,
-        questions: list
-      }
+        questions: list,
+      },
     };
   }
 
-  onChange = (e) => {
-    var amt = (parseInt(e.target.value)) ? parseInt(e.target.value) : 10;
-    var old_amt = this.state.form_data.amt
+  onChange = e => {
+    var amt = parseInt(e.target.value) ? parseInt(e.target.value) : 10;
+    var old_amt = this.state.form_data.amt;
     if (amt > 100 || amt < 1) {
-      amt = 10
+      amt = 10;
     }
     var questions = [];
-    var old_list = this.state.form_data.questions
+    var old_list = this.state.form_data.questions;
     for (var i = 1; i < amt + 1; i++) {
       if (i - 1 < old_amt) {
-        questions.push(old_list[i - 1])
+        questions.push(old_list[i - 1]);
       } else {
         questions.push({
           id: i,
-          questionType: "single",
-          question: "",
-          answerOne: "",
-          answerTwo: "",
-          optionOne: "",
-          optionTwo: "",
-          optionThree: ""
-        })
+          questionType: 'single',
+          question: '',
+          answerOne: '',
+          answerTwo: '',
+          optionOne: '',
+          optionTwo: '',
+          optionThree: '',
+        });
       }
     }
     this.setState({
       questionAmtSelectedValue: amt,
       form_data: {
         amt: amt,
-        questions: questions
-      }
+        questions: questions,
+      },
     });
-  }
+  };
 
-  handleQuestion = (_state) => {
-    var index = _state.id
-    var old_list = this.state.form_data.questions
-    var old_obj = old_list[index]
-    var amt = this.state.form_data.amt
-    old_obj.question = _state.question
-    old_obj.questionType = _state.questionType
-    old_obj.answerOne = _state.answerOne
-    old_obj.answerTwo = _state.answerTwo
-    old_obj.optionOne = _state.optionOne
-    old_obj.optionTwo = _state.optionTwo
-    old_obj.optionThree = _state.optionThree
+  handleQuestion = _state => {
+    var index = _state.id;
+    var old_list = this.state.form_data.questions;
+    var old_obj = old_list[index];
+    var amt = this.state.form_data.amt;
+    old_obj.question = _state.question;
+    old_obj.questionType = _state.questionType;
+    old_obj.answerOne = _state.answerOne;
+    old_obj.answerTwo = _state.answerTwo;
+    old_obj.optionOne = _state.optionOne;
+    old_obj.optionTwo = _state.optionTwo;
+    old_obj.optionThree = _state.optionThree;
     this.setState({
       form_data: {
         amt: amt,
-        questions: old_list
-      }
-    })
-  }
+        questions: old_list,
+      },
+    });
+  };
 
   submitForms = () => {
-    console.log(this.state)
-  }
+    console.log(this.state);
+  };
 
   render() {
     const { classes } = this.props;
     return (
       <div>
-        <form id="data">
+        <form id='data'>
           <TextField
-            id="title"
-            label="Title"
+            id='title'
+            label='Title'
             className={classes.textField}
-            margin="normal"
-            variant="standard"
+            margin='normal'
+            variant='standard'
             inputProps={{
-              style: {fontSize: 30} 
+              style: { fontSize: 30 },
             }}
           />
           <br />
           <TextField
-            id="outlined-name"
-            label="Description"
+            id='outlined-name'
+            label='Description'
             className={classes.textField}
-            margin="normal"
-            variant="outlined"
+            margin='normal'
+            variant='outlined'
           />
           <TextField
-            id="outlined-name"
-            label="Class"
+            id='outlined-name'
+            label='Class'
             className={classes.textField}
-            margin="normal"
-            variant="outlined"
+            margin='normal'
+            variant='outlined'
           />
           <br />
-          <TextField name="question_amt" label="Amount of Questions" className={classes.textField} margin="normal" variant="outlined" onChange={this.onChange} />
-        </form><br />
+          <TextField
+            name='question_amt'
+            label='Amount of Questions'
+            className={classes.textField}
+            margin='normal'
+            variant='outlined'
+            onChange={this.onChange}
+          />
+        </form>
+        <br />
         <Card className={classes.card}>
           <CardContent>
-            {Array.from(Array(this.state.questionAmtSelectedValue).keys()).map((e) => <CreateQuestionObject index={e + 1} handleQuestion={this.handleQuestion} />)}
+            {Array.from(Array(this.state.questionAmtSelectedValue).keys()).map(
+              e => (
+                <CreateQuestionObject
+                  index={e + 1}
+                  handleQuestion={this.handleQuestion}
+                />
+              ),
+            )}
           </CardContent>
         </Card>
-        <input type="submit" className="button" value="Submit" onClick={this.submitForms()} />
-
+        <input
+          type='submit'
+          className='button'
+          value='Submit'
+          onClick={this.submitForms()}
+        />
       </div>
-
     );
   }
 }
