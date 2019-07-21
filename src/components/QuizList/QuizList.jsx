615b891b7d761fff3c19f033397f4c60f7d4b0ba
Robert Hinman <rhinman@pandora.com>
2019-07-21 13:35:02 -0700

    Got QuizList loading from store

--- src/components/QuizList/QuizList.jsx
+++ src/components/QuizList/QuizList.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import DataList from '../DataList/DataList';
-import displayData from '../../actions/quizActions';
-import store from '../../reducers/index.js';
+import React from "react";
+import DataList from "../DataList/DataList";
+// import { displayQuiz } from "../../actions/quizActions";
+import { connect } from "react-redux";
+// import store from "../../reducers/index.js";
 
 class QuizList extends React.Component {
   render() {
     const headers = ["Quiz Name", "Author", "Create Date", "Action"];
-    const quizzes = this.props.data.quizzes;
+    const quizzes = this.props.quizzes;
     const actions = ["Edit", "Assign"];
-    store.dispatch(displayData('["Quiz 4", "John Smith", "11/7/18"]'));
+    // store.dispatch(displayQuiz('["Quiz 4", "John Smith", "11/7/18"]'));
 
-    return (
-      <DataList headers={ headers } rows={ quizzes } actions={ actions }/>
-    );
+    return <DataList headers={headers} rows={quizzes} actions={actions} />;
   }
 }
 
-export default QuizList;
+const mapStateToProps = state => ({
+  quizzes: state.dashboardQuizList,
+});
+
+export default connect(mapStateToProps)(QuizList);
