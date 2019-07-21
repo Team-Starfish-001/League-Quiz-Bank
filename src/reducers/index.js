3351ec3e44a3420b5cbd52a69bb78c7c7f145586
Robert Hinman <rhinman@pandora.com>
2019-07-21 13:36:20 -0700

    Now have data in quizlist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,18 @@
-import * as actions from '../actions/actionConstants';
+import * as actions from "../actions/actionConstants";
 // import { createStore } from 'redux';
 
 const initialState = {
-  dashboardQuizList: [],
+  dashboardQuizList: [
+    ["Quiz 1", "John Smith", "10/25/18"],
+    ["Quiz 2", "John Smith", "10/32/18"],
+    ["Quiz 3", "John Smith", "11/7/18"],
+    ["Quiz 4", "John Smith", "11/7/18"],
+  ],
 };
 
 function quizReducer(state = initialState, action) {
   switch (action.type) {
-  case actions.SET_QUIZLIST: 
+  case actions.SET_QUIZLIST:
     return Object.assign({}, state, { dashboardQuizList: action.payload });
   case actions.DISPLAY_QUIZ:
     return Object.assign({}, state, { dashboardQuizList: action.payload });
