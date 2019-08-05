93de9e5a6e54666ef5587baa7bb2788e11becaef
Robert Hinman <rhinman@pandora.com>
2019-08-05 10:57:41 -0700

    removed console log

--- src/components/createQuestionObject/CreateQuestionObject.jsx
+++ src/components/createQuestionObject/CreateQuestionObject.jsx
@@ -35,7 +35,6 @@ const style_css = {
 
 class CreateQuestionObject extends Component {
   render() {
-    console.log(this.props);
     const questionIndex = this.props.questionIndex;
     return (
       <div>
