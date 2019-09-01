1930dc200456c7ad089d96661ebc342af5f31a4c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-01 21:09:30 +0000

    Deploy Team-Starfish-001/League-Quiz-Bank to github.com/Team-Starfish-001/League-Quiz-Bank.git:gh-pages

---
+++ precache-manifest.7c74a669bf123e6d7884ac854428e180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6d9ad0b3d85da6f124e6486671580e",
+    "url": "/League-Quiz-Bank/index.html"
+  },
+  {
+    "revision": "bd3eef7eb6fcc04d7bbd",
+    "url": "/League-Quiz-Bank/static/css/main.eb94d447.chunk.css"
+  },
+  {
+    "revision": "8324c28a3ed436305e69",
+    "url": "/League-Quiz-Bank/static/js/2.78446346.chunk.js"
+  },
+  {
+    "revision": "bd3eef7eb6fcc04d7bbd",
+    "url": "/League-Quiz-Bank/static/js/main.81fcef44.chunk.js"
+  },
+  {
+    "revision": "3901452ed327453dec81",
+    "url": "/League-Quiz-Bank/static/js/runtime~main.b1b7d8d2.js"
+  }
+]);
